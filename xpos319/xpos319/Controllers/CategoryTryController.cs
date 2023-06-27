644e2bc37349a25fe2168ad7e6e5f2fb7e779573
ujangdotnet <ujangdotnet@gmail.com>
2023-06-27 16:48:30 +0700

    day 4

--- xpos319/xpos319/Controllers/CategoryTryController.cs
+++ xpos319/xpos319/Controllers/CategoryTryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using xpos319.datamodels;
+using xpos319.Models;
 using xpos319.Services;
 using xpos319.viewmodels;
 
@@ -27,6 +28,77 @@ namespace xpos319.Controllers
 			VMTblCategory dataView = new VMTblCategory();
 
 			return View(dataView);
+		}
+
+		[HttpPost]
+		public IActionResult Create(VMTblCategory dataView)
+		{
+			VMResponse respon = new VMResponse();
+
+			if(ModelState.IsValid)
+			{
+				respon = categoryTryService.Create(dataView);
+
+				if (respon.Success)
+				{
+					return RedirectToAction("Index");
+				}
+			}
+
+			respon.Entity = dataView;
+			return View(respon.Entity);
+		}
+
+		public IActionResult Edit(int id)
+		{
+			VMTblCategory dataView = categoryTryService.GetById(id);
+			return View(dataView);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(VMTblCategory dataView)
+		{
+            VMResponse respon = new VMResponse();
+
+            if (ModelState.IsValid)
+            {
+                respon = categoryTryService.Edit(dataView);
+
+                if (respon.Success)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            respon.Entity = dataView;
+            return View(respon.Entity);
+        }
+
+		//detail
+		public IActionResult Detail(int id)
+		{
+            VMTblCategory dataView = categoryTryService.GetById(id);
+            return View(dataView);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            VMTblCategory dataView = categoryTryService.GetById(id);
+            return View(dataView);
+        }
+
+		[HttpPost]
+		public IActionResult Delete(VMTblCategory dataView)
+		{
+			VMResponse respon = new VMResponse();
+			respon = categoryTryService.Delete(dataView);
+
+				if (respon.Success)
+				{
+					return RedirectToAction("Index");
+				}
+
+			return View(respon.Entity);
 		}
 	}
 }
--- xpos319/xpos319/Services/CategoryTryService.cs
+++ xpos319/xpos319/Services/CategoryTryService.cs
@@ -4,10 +4,11 @@ using xpos319.viewmodels;
 
 namespace xpos319.Services
 {
-	public class CategoryTryService
-	{
-		private readonly XPOS_319Context db; //pembuatan line ini untuk apa ?
-		VMResponse respon = new VMResponse(); //ini juga buat apa ?
+    public class CategoryTryService
+    {
+        private readonly XPOS_319Context db; //pembuatan line ini untuk apa ?
+        VMResponse respon = new VMResponse(); //ini juga buat apa ?
+        int IdUser = 1;
 
         public CategoryTryService(XPOS_319Context _db)
         {
@@ -34,5 +35,115 @@ namespace xpos319.Services
 
             return dataView;
         }
-    }
+
+        public VMResponse Create(VMTblCategory dataView)
+        {
+
+            TblCategory dataModel = GetMapper().Map<TblCategory>(dataView);
+            dataModel.CreateBy = IdUser;
+            dataModel.CreateDate = DateTime.Now;
+            dataModel.IsDelete = false;
+
+            try
+            { 
+                db.Add(dataModel);
+                db.SaveChanges();
+
+                respon.Message = "Data Saved";
+                respon.Entity = dataModel;
+            }
+            catch (Exception e)
+            {
+                respon.Success = false;
+                respon.Message = "Failed Save Data: " + e.Message;
+                respon.Entity = dataModel;
+            }
+
+            return respon;
+        }
+
+        public VMTblCategory GetById(int id)
+        {
+            TblCategory dataModel = db.TblCategories.Find(id);
+            VMTblCategory dataView = GetMapper().Map<VMTblCategory>(dataModel);
+
+            return dataView;
+        }
+
+        public VMResponse Edit(VMTblCategory dataView)
+        {
+            TblCategory dataModel = db.TblCategories.Find(dataView.Id);
+            dataModel.NameCategory = dataView.NameCategory;
+            dataModel.Description = dataView.Description;
+            dataModel.UpdateBy = IdUser;
+            dataModel.UpdateDate = DateTime.Now;
+
+            try
+            {
+                db.Update(dataModel); //fuction update darimana ?
+                db.SaveChanges(); //fuction save changes darimana ?
+
+                respon.Message = "Data Success updated";
+                respon.Entity = GetMapper().Map<VMTblCategory>(dataModel); //fuction getmapper() darimana ?
+
+            }
+            catch(Exception e)
+            {
+                respon.Success = false;
+                respon.Message = "Failed edited: " + e.Message;
+                respon.Entity = GetMapper().Map<VMTblCategory>(dataModel);
+            }
+
+            return respon;
+        }
+
+		public VMResponse Delete(VMTblCategory dataView)
+		{
+			TblCategory dataModel = db.TblCategories.Find(dataView.Id);
+            dataModel.IsDelete = true;
+            dataModel.UpdateBy = IdUser;
+            dataModel.UpdateDate = DateTime.Now;
+
+            try
+            {
+                db.Update(dataModel);
+                db.SaveChanges();
+
+				respon.Message = "Data Success removed";
+				respon.Entity = GetMapper().Map<VMTblCategory>(dataModel); //fuction getmapper() darimana ?
+
+			}
+			catch (Exception e)
+            {
+				respon.Success = false;
+				respon.Message = "Failed removed: " + e.Message;
+				respon.Entity = GetMapper().Map<VMTblCategory>(dataModel);
+			}
+
+            return respon;
+
+			//dataModel.NameCategory = dataView.NameCategory;
+			//dataModel.Description = dataView.Description;
+			//dataModel.UpdateBy = IdUser; 
+			//dataModel.UpdateDate = DateTime.Now;
+
+			//try
+			//{
+			//	db.Remove(dataModel); //fuction update darimana ?
+			//	db.SaveChanges(); //fuction save changes darimana ?
+
+			//	respon.Message = "Data Success removed";
+			//	respon.Entity = GetMapper().Map<VMTblCategory>(dataModel); //fuction getmapper() darimana ?
+
+			//}
+			//catch (Exception e)
+			//{
+			//	respon.Success = false;
+			//	respon.Message = "Failed removed: " + e.Message;
+			//	respon.Entity = GetMapper().Map<VMTblCategory>(dataModel);
+			//}
+
+			//return respon;
+		}
+	}
 }
